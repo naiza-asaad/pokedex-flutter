eb3c86ba6b04e3f3a5b07c8f078c6f0ec8eb61d7
Naiza Asaad <naizaoasaad@gmail.com>
2021-03-28 09:48:52 +0800

    Fix bug flavor text entry is sometimes not in English

--- lib/models/pokemon_species.dart
+++ lib/models/pokemon_species.dart
@@ -8,7 +8,20 @@ class PokemonSpecies {
   });
 
   factory PokemonSpecies.fromJson(Map<String, dynamic> parsedJson) {
-    final flavorText = (parsedJson['flavor_text_entries'][0]['flavor_text'] as String).replaceAll('\n', ' ').replaceAll('\u000c', ' ');
+    var tempFlavorTextList = parsedJson['flavor_text_entries'] as List;
+    // Always fetches the first English flavor text entry.
+    // The first element of the list is usually, BUT NOT ALWAYS, the first English entry.
+    // So, check for the actual language first.
+    final firstEnglishFlavorTextEntryIndex = tempFlavorTextList
+        .indexWhere((element) => element['language']['name'] as String == 'en');
+
+    // Removes flavor text entries' escape sequences ('\n' and '\u000c') in between sentences.
+    // These are returned by the API to probably copy the original look of the actual Pokedex inside the games.
+    final flavorText = (parsedJson['flavor_text_entries']
+            [firstEnglishFlavorTextEntryIndex]['flavor_text'] as String)
+        .replaceAll('\n', ' ')
+        .replaceAll('\u000c', ' ');
+
     return PokemonSpecies(
       evolutionChainUrl: parsedJson['evolution_chain']['url'],
       flavorTextEntry1: flavorText,
